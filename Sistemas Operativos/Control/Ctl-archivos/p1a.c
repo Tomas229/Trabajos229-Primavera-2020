76045ae306f5b6ae06d0cf34ea83fe4e30f9d35a
Tomas229 <53621992+Tomas229@users.noreply.github.com>
2021-02-10 13:11:58 -0300

    archivos Control completos

---
+++ Sistemas Operativos/Control/Ctl-archivos/p1a.c
@@ -0,0 +1,15 @@
+#include <nSystem.h>
+
+... variables globales ...
+
+void init() {
+  ... inicialice aca las variables globales ....
+}
+
+void pedir(int cat) {
+  ...
+}
+
+void devolver() {
+  ...
+}
---
+++ Sistemas Operativos/Control/Ctl-archivos/p1b.c
@@ -0,0 +1,18 @@
+#include <nSystem.h>
+#include <fifoqueues.h>
+
+... variables globales ...
+
+... defina estructuras ...
+
+void init() {
+  ... inicialice variables globales ...
+}
+
+void pedir(int cat) {
+  ...
+}
+
+void devolver() {
+  ...
+}
